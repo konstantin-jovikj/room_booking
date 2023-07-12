5cce9c3564538f377e2a31a36a3208bac7dc5694
konstantin-jovikj <jovichmk@gmail.com>
2023-07-12 14:03:20 +0200

    created admin middleware

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,6 @@ use App\Http\Livewire\EditRoom;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 
 Route::get('/', [LayoutController::class, 'index'])->name('home.page');
@@ -41,8 +38,24 @@ Route::get('/dashboard', function () {
 // })->middleware(['auth', 'verified'])->name('admin.dashboard');
 
 
-Route::middleware(['auth', 'verified'])->group(function(){
+Route::middleware(['auth', 'verified', 'admin'])->group(function(){
     Route::get('/dashboard/admin',[AdminController::class, 'index'])->name('admin.dashboard');
+
+    Route::get('/buildings', [BuildingController::class, 'index'])->name('index.buildings');
+    Route::get('/building/create', [BuildingController::class, 'create'])->name('create.building');
+    Route::get('/building/show/{building}', [BuildingController::class, 'show'])->name('view.building');
+    Route::delete('/building/delete/{building}', [BuildingController::class, 'destroy'])->name('delete.building');
+    Route::get('/building/edit/{building}', EditBuilding::class)->name('edit.building');
+    Route::get('/building/image/{building}',[BildingImagesController::class,'create'])->name('create.buildingimage');
+    Route::post('/building/image/store/{building}', [BildingImagesController::class,'store'])->name('store.buildingimage');
+    Route::delete('building/image/delete/{bildingImages}', [BildingImagesController::class, 'destroy'])->name('delete.buildingimage');
+    Route::get('/room/create', [RoomController::class, 'create'])->name('create.room');
+    Route::post('/room/store/',[RoomController::class,'store'])->name('store.room');
+    Route::get('/room/image/{room}',[RoomImageController::class,'create'])->name('create.roomimage');
+    Route::post('/room/image/store/{room}', [RoomImageController::class,'store'])->name('store.roomimage');
+    Route::delete('room/image/delete/{roomImage}', [RoomImageController::class, 'destroy'])->name('delete.roomimage');
+    Route::delete('/room/delete/{room}', [RoomController::class, 'destroy'])->name('delete.room');
+    Route::get('/room/edit/{room}', EditRoom::class)->name('edit.room');
 });
 
 
@@ -53,24 +66,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/buildings', [BuildingController::class, 'index'])->name('index.buildings');
-    Route::get('/building/create', [BuildingController::class, 'create'])->name('create.building');
-    Route::get('/building/show/{building}', [BuildingController::class, 'show'])->name('view.building');
-    Route::delete('/building/delete/{building}', [BuildingController::class, 'destroy'])->name('delete.building');
-    Route::get('/building/edit/{building}', EditBuilding::class)->name('edit.building');
 
-    Route::get('/building/image/{building}',[BildingImagesController::class,'create'])->name('create.buildingimage');
-    Route::post('/building/image/store/{building}', [BildingImagesController::class,'store'])->name('store.buildingimage');
-    Route::delete('building/image/delete/{bildingImages}', [BildingImagesController::class, 'destroy'])->name('delete.buildingimage');
 
-    Route::get('/room/create', [RoomController::class, 'create'])->name('create.room');
-    Route::post('/room/store/',[RoomController::class,'store'])->name('store.room');
-    Route::get('/room/image/{room}',[RoomImageController::class,'create'])->name('create.roomimage');
-    Route::post('/room/image/store/{room}', [RoomImageController::class,'store'])->name('store.roomimage');
-    Route::delete('room/image/delete/{roomImage}', [RoomImageController::class, 'destroy'])->name('delete.roomimage');
 
-    Route::delete('/room/delete/{room}', [RoomController::class, 'destroy'])->name('delete.room');
-    Route::get('/room/edit/{room}', EditRoom::class)->name('edit.room');
     Route::get('/room/book/{room}', [BookingController::class, 'bookRoom'])->name('book.room');
     Route::post('/room/booking/store/{room}', [BookingController::class, 'store'])->name('store.room.booking');
     Route::get('/mybookings', [BookingController::class, 'userBookings'])->name('user.bookings');
